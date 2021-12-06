fd5b0dba46baf213561487b02729b3e1fd0404f8
Cody <33189090+cdubles@users.noreply.github.com>
2021-12-06 10:41:14 -0500

    add password check in signup

--- public/client.js
+++ public/client.js
@@ -25,21 +25,28 @@ $(function () {
 
   //sign up
   $("button#signUpSubmit").click(function () {
-    const payload = {
-      body: JSON.stringify({
-        Fname: $("input#Fname").val(),
-        Lname: $("input#Lname").val(),
-        username: $("input#signUsername").val(),
-        email: $("input#email").val(),
-        password: $("input#signPassword").val(),
-      }),
-      method: "post",
-      headers: {
-        "content-type": "application/json",
-      },
-    };
-    fetch("/signup", payload)
-      .then((res) => res.json())
-      .then((res) => console.log(res));
+    if($("input#signPassword").val() == $("input#passwordConfirm").val()){
+      //passwords match
+      const payload = {
+        body: JSON.stringify({
+          Fname: $("input#Fname").val(),
+          Lname: $("input#Lname").val(),
+          username: $("input#signUsername").val(),
+          email: $("input#email").val(),
+          password: $("input#signPassword").val(),
+        }),
+        method: "post",
+        headers: {
+          "content-type": "application/json",
+        },
+      };
+      fetch("/signup", payload)
+        .then((res) => res.json())
+        .then((res) => console.log(res));
+    }
+    else{
+      alert('passwords do not match')
+    }
+    
   });
 });
